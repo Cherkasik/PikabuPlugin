7ad87d49163249ef08db4c4c975b1edd6ce219d3
Cherkasik <ilyakopeysk@gmail.com>
2019-01-16 02:57:34 +0300

    don't know how to add ico, at least working

--- src/main/java/PikabuMem.java
+++ src/main/java/PikabuMem.java
@@ -7,15 +7,15 @@ import java.util.Random;
 
 public class PikabuMem extends AnAction {
     public PikabuMem() {
-        super(null, null, new ImageIcon(PikabuMem.class.getClassLoader().getResource("icon/pikabu.png")));
+        super(null, null, new ImageIcon(PikabuMem.class.getClassLoader().getResource("icon/pikabu.ico")));
     }
 
     public void actionPerformed(AnActionEvent event) {
         String pikabuSite = "https://www.pikabu.ru/best/";
         Random random = new Random();
-        int dateDay = random.nextInt(29) + 1;
-        int dateMonth = random.nextInt(13) + 1;
-        int dateYear = random.nextInt(2020) + 2008;
+        int dateDay = random.nextInt(27) + 1;
+        int dateMonth = random.nextInt(11) + 1;
+        int dateYear = random.nextInt(12) + 2008;
         BrowserUtil.browse(pikabuSite + dateDay + "-" + dateMonth + "-" + dateYear);
     }
 }
