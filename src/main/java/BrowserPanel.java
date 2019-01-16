c58779bc2d5dee69a47896d2d70ddacaf65c3154
Cherkasik <ilyakopeysk@gmail.com>
2019-01-16 19:27:59 +0300

    based on emBrowser working page of pikabu To start watching memes push the button To get new random page push the button again

--- src/main/java/BrowserPanel.java
+++ src/main/java/BrowserPanel.java
@@ -1,6 +1,92 @@
-import javax.swing.*;
+import javafx.application.Platform;
 
-public class BrowserPanel extends JComponent {
-    public BrowserPanel(JavaFxBrowserView javaFxBrowserView) {
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+/**
+ * Main panel component
+ */
+public class BrowserPanel extends JPanel {
+    private BrowserView browserView;
+    public JButton buttonGo;
+
+    /**
+     * @param browserView BrowserView
+     */
+    public BrowserPanel(BrowserView browserView) {
+        this.browserView = browserView;
+        initWebView();
+    }
+
+    public JPanel getTopControllers() {
+        JPanel topControlPanel = new JPanel();
+        GridBagLayout topControlPanelLayout = new GridBagLayout();
+
+        topControlPanel.setLayout(topControlPanelLayout);
+
+        buttonGo = new JButton("");
+        buttonGo.setPreferredSize(new Dimension(40, 30));
+
+        topControlPanel.add(buttonGo);
+
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
+        gridBagConstraints.gridwidth = 1;
+        gridBagConstraints.weightx = 0;
+        gridBagConstraints.weighty = 0;
+        topControlPanelLayout.setConstraints(buttonGo, gridBagConstraints);
+
+        buttonGo.setEnabled(true);
+
+        buttonGo.addActionListener(event -> this.webBrowserLoad());
+
+        return topControlPanel;
+    }
+
+    private void webBrowserLoad()
+    {
+        String pikabuSite = "https://www.pikabu.ru/best/";
+        Random random = new Random();
+        int dateDay = random.nextInt(27) + 1;
+        int dateMonth = random.nextInt(11) + 1;
+        int dateYear = random.nextInt(12) + 2008;
+        browserView.load(pikabuSite + dateDay + "-" + dateMonth + "-" + dateYear);
+    }
+
+    private void initWebView() {
+        Platform.setImplicitExit(false);
+        SwingUtilities.invokeLater(() -> {
+            removeAll();
+            GridBagLayout layout = new GridBagLayout();
+            setLayout(layout);
+
+            JComponent topControllers = getTopControllers();
+            add(topControllers);
+
+            browserView.init();
+            browserView.setPanel(this);
+
+            JComponent webPanel = browserView.getNode();
+            add(webPanel);
+
+            GridBagConstraints gridBagConstraints = new GridBagConstraints();
+            gridBagConstraints.fill = GridBagConstraints.BOTH;
+
+            gridBagConstraints.gridwidth = 0;
+            gridBagConstraints.weightx = 1;
+            gridBagConstraints.weighty = 0;
+            layout.setConstraints(topControllers, gridBagConstraints);
+
+            gridBagConstraints.gridwidth = 0;
+            gridBagConstraints.weightx = 1;
+            gridBagConstraints.weighty = 10;
+            layout.setConstraints(webPanel, gridBagConstraints);
+
+            validate();
+            repaint();
+
+            buttonGo.setEnabled(true);
+        });
     }
 }
--- src/main/java/BrowserWindow.java
+++ src/main/java/BrowserWindow.java
@@ -10,19 +10,36 @@ import com.intellij.ui.content.ContentFactory;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 
+/**
+ * Tool Window Factory
+ */
 public class BrowserWindow implements ToolWindowFactory {
-    public BrowserWindow(){}
-    public void createToolWindowContent(Project project, ToolWindow toolWindow){
+
+    /**
+     *  Constructor
+     */
+    public BrowserWindow() {
+
+    }
+
+    /**
+     * @param project Project
+     * @param toolWindow ToolWindow
+     */
+    public void createToolWindowContent(Project project, ToolWindow toolWindow) {
+
         final ToolWindowManager manager = ToolWindowManager.getInstance(project);
+
         ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
         Content content = contentFactory.createContent(new BrowserPanel(new JavaFxBrowserView()),"", false);
         toolWindow.getContentManager().addContent(content);
-    toolWindow.getContentManager().getComponent().addComponentListener(new ComponentListener() {
-        public void componentResized(ComponentEvent e) {}
-        public void componentMoved(ComponentEvent e) { }
-        public void componentShown(ComponentEvent e) {}
-        public void componentHidden(ComponentEvent e) {}
-    });
+        toolWindow.getContentManager().getComponent().addComponentListener(new ComponentListener() {
+            public void componentResized(ComponentEvent e) {}
+            public void componentMoved(ComponentEvent e) { }
+            public void componentShown(ComponentEvent e) {}
+            public void componentHidden(ComponentEvent e) {}
+        });
+
         final ToolWindowManagerListener listener = new ToolWindowManagerListener() {
             public void toolWindowRegistered(String id) {}
             public void stateChanged() {}
